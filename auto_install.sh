7b43f617264d6e432d8a5ee33d74f703f325c982
fibreyu <fibreyu@gmail.com>
2022-06-14 17:14:51 +0800

    change download url

--- auto_install.sh
+++ auto_install.sh
@@ -4,5 +4,5 @@ apt update -y
 apt install dkms -y
 apt install pve-headers-$(uname -r)
 
-wget https://files.gitee.com/group1/M00/2A/41/CgAAEmKoM0qAbCe0AAFmIIVWJpw326.deb?token=d3bfd41b5d0403a6b61c41e450b7bbf4&ts=1655190912&attname=pve-realtek-r8125-dkms_9.009.01-1_amd64.deb&disposition=attachment
+wget https://gitee.com/fibreyu/pve_realtek_r8125_dkms/attach_files/1094653/download/pve-realtek-r8125-dkms_9.009.01-1_amd64.deb
 dpkg -i pve-realtek-r8125-dkms_9.009.01-1_amd64.deb
