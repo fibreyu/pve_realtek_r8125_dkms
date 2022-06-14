5804dc1c6eac6e3c695f0bcc7aee56227339c919
fibreyu <fibreyu@gmail.com>
2022-06-14 18:13:06 +0800

    fix bug

--- auto_install.sh
+++ auto_install.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
 apt update -y
-apt install dkms -y
-apt install pve-headers-$(uname -r)
+apt install -y dkms
+apt install -y pve-headers-$(uname -r)
 
 wget https://gitee.com/fibreyu/pve_realtek_r8125_dkms/attach_files/1094653/download/pve-realtek-r8125-dkms_9.009.01-1_amd64.deb
 dpkg -i pve-realtek-r8125-dkms_9.009.01-1_amd64.deb
+
+modprobe r8125
+sed -i "s/r8125//g" /etc/modules
+echo "r8125" >> /etc/modules
