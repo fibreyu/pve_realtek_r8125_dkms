d4b3c482a228601870eff07d9ecdb7f97fdb33e9
fibreyu <fibreyu@gmail.com>
2022-06-22 23:54:42 +0800

    change url

--- auto_install.sh
+++ auto_install.sh
@@ -4,7 +4,7 @@ apt update -y
 apt install -y dkms
 apt install -y pve-headers-$(uname -r)
 
-wget https://gitee.com/fibreyu/pve_realtek_r8125_dkms/attach_files/1094653/download/pve-realtek-r8125-dkms_9.009.01-1_amd64.deb -O driver.deb
+wget https://gitee.com/fibreyu/pve_realtek_r8125_dkms/attach_files/1104455/download/pve-realtek-r8125-dkms_9.009.01-1_amd64.deb -O driver.deb
 dpkg -i driver.deb
 
 rm -rf driver.deb
