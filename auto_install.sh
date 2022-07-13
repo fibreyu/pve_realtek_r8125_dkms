e87635a6760588d57d2d58a9564e694e9da66a1b
fibreyu <fibreyu@gmail.com>
2022-07-14 00:43:37 +0800

    gix bug

--- auto_install.sh
+++ auto_install.sh
@@ -4,11 +4,11 @@ apt update -y
 apt install -y dkms
 apt install -y pve-headers-$(uname -r)
 
-wget https://gitee.com/fibreyu/pve_realtek_r8125_dkms/attach_files/1126173/download/pve-realtek-r8125-dkms_9.009.02-1_amd64.deb -O /tmp/driver.deb
+wget https://gitee.com/fibreyu/pve_realtek_r8125_dkms/attach_files/1126173/download/pve-realtek-r8125-dkms_9.009.02-1_amd64.deb -O driver.deb
 
 # apt install -f /tmp/driver.deb -y
-dpkg -i driver.deb
+dpkg -i ./driver.deb
 
-rm -rf /tmp/driver.deb
+rm -rf ./driver.deb
 
 echo "reboot to apply new driver!"
